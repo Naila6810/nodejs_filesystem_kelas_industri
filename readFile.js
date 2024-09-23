9f4ac435348cf66a6bf17efa06a082a5e16f2c4a
Naila6810 <nailafitrianissa34@smk.belajar.id>
2024-09-23 14:14:45 +0700

    Biodata Naila

---
+++ readFile.js
@@ -0,0 +1,10 @@
+const fs = require('node:fs');
+fs.readFile('Biodata.txt', 'utf8', 
+    
+function (eror, data) {
+  if (eror) {
+    console.eror(eror);
+  } else {
+    console.log('isi filenya :', data);
+  }
+});
